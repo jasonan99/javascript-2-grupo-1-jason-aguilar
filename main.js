400c72fee30c265a3e2fe1f8032d1d502389ea69
jasonan99 <106450277+jasonan99@users.noreply.github.com>
2023-01-17 18:51:11 -0600

    add prueba-diagnostico

---
+++ main.js
@@ -0,0 +1,120 @@
+// Prueba Diagnostico
+
+// I Parte: Teoría
+
+// 1. Respuesta pregunta 1
+
+/* 
+    number: 1
+    string: hola
+    boolean: true
+*/
+
+// ====================================================================================================
+
+
+// 2. Respuesta pregunta 2
+
+/* 
+    object.property = value Ej: person.name = 'Pedro';
+    object['property'] = value Ej: person['name'] = 'Pedro';
+*/
+
+// ====================================================================================================
+
+
+// 3. Respuesta pregunta 3
+
+/* 
+    Le suma a list lo que tiene mas lo que esta despues del igual.
+    El primer console.log mostraria: 4465
+    El segundo console.log mostraria: 4465textoDePrueba
+*/
+
+// ====================================================================================================
+
+
+// 4. Respuesta pregunta 4
+
+/* 
+    != significa no es igual a.
+    !== significa no es estrictamente igual a.
+    La diferencia es que != no hace conversion de datos mientras que !== si.
+
+    A. true, true.
+    B. false, true.
+*/
+
+// ====================================================================================================
+
+
+// 5. Respuesta pregunta 5
+
+/* 
+    Son las que no han sido declaradas con un nombre.
+*/
+
+// ====================================================================================================
+
+
+// 6. Respuesta pregunta 6
+
+/* 
+    Las function declaration tienen que tener un nombre mientras que 
+    las function expression pueden ser anonimas y se ejecutaran apenas
+    sean definidas.
+*/
+
+// ====================================================================================================
+
+
+// 7. Respuesta pregunta 7
+
+/* 
+    Son funciones que pertenecen a un objeto.
+*/
+
+// ====================================================================================================
+
+
+// 8. Respuesta pregunta 8
+
+/* 
+    Es una funcion que se llama a si misma hasta que la condicion le indica
+    que deje de hacerlo. 
+*/
+
+// ====================================================================================================
+
+
+// II Parte: Ejercicios
+
+// 1. Respuesta pregunta 1
+
+/* 
+    let lista = [1,2,3,4,5,6];
+    let pares = [];
+    let impares = [];
+
+    function ParesImpares(lista, pares, impares) {
+    for (i = 0; i < lista.length; i++) {
+        if(lista[i] % 2 == 0) {
+            pares.push(lista[i]);
+        } else {
+            impares.push(lista[i]);
+        }
+    }
+        console.log("Los numeros pares son: " + pares + ", y los numeros impares son: " + impares + "."); 
+    }
+
+    ParesImpares(lista, pares, impares);
+*/
+
+// ====================================================================================================
+
+
+// 2. Respuesta pregunta 2
+
+// Colocar el código acá
+
+// ====================================================================================================
