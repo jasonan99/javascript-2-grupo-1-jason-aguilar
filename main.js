29d34b464fef97f348ea30f59abfc1b49de7d4a9
jasonan99 <106450277+jasonan99@users.noreply.github.com>
2023-03-18 03:02:14 -0600

    upload mini proyecto segunda entrega

--- main.js
+++ main.js
@@ -1,157 +1,171 @@
-class Bookstore {
-  #bookstoreName;
-  #address;
-  #books;
-  #comics;
-  constructor(bookstoreName, address) {
-    this.#bookstoreName = bookstoreName;
-    this.#address = address;
-    this.#books = [];
-    this.#comics = [];
+import Bookstore from './modules/Bookstore.js';
+import Book from './modules/Book.js';
+import Comic from './modules/Comic.js';
+
+(function () {
+  const bookstore1 = new Bookstore('Libreria 1', 'Calle 1');
+  const sidebar = document.getElementById('sidebar');
+  const mainContent = document.getElementById('main-content');
+  const menu = ['Show Books', 'Show Comics', 'Add Book', 'Add Comic'];
+
+  const initialize = () => {
+    printBookstoreName();
+    displayMenu();
+    menuFunctionality();
   }
-
-
-  // getters
-  get getBooks() {
-    return this.#books;
-  }
-
-  get getComics() {
-    return this.#comics;
-  }
-
-  get getName() {
-    return this.#bookstoreName
-  }
-
-  // setters
-  set setBookstoreName(name) {
-    this.#bookstoreName = name;
-  }
-
-  set setAddress(address) {
-    this.#address = address;
-  }
-
-  set setBooks(book) {
-    this.#books.push(book);
-  }
-
-  set setComics(comic) {
-    this.#comics.push(comic);
-  }
-}
-
-class Book {
-  #title;
-  #author;
-  #price;
-  #stock;
-  #year;
   
-  constructor(title, author, price, stock, year) {
-    this.#title = title;
-    this.#author = author;
-    this.#price = price;
-    this.#stock = stock;
-    this.#year = year;
-  }
-
-  getInfo() {
-    return {
-      title: this.#title,
-      auhor: this.#author,
-      price: this.#price,
-      year: this.#year,
-      stock: !this.#stock ? 'No hay ejemplares disponibles' : this.#stock
-    }
-  }
-
-  // setters
-  set setStock(stock) {
-    this.#stock = stock;
-  }
-
-  set setYear(year) {
-    this.#year = year;
-  }
-}
-
-class Comic extends Book {
-  #illustrator;
-  #publisher;
-  #volume;
-  constructor(title, author, price, stock, year, illustrator, publisher, volume) {
-    super(title, author, price, stock, year);
-    this.#illustrator = illustrator;
-    this.#publisher = publisher;
-    this.#volume = volume;
-  }
-
-  getInfo () {
-    const info = super.getInfo(); 
-    return {
-      ...info,
-      illustrator: this.#illustrator,
-      publisher: this.#publisher,
-      volume: this.#volume
-    }
-  }
-}
-
-const bookstore1 = new Bookstore('Libreria 1', 'Calle 1');
-
-// const addBook = () => {
-//   const book = new Book('El principito', 'Antoine de Saint', 100, 10, 1943);
-//   bookstore1.setBooks = book.getInfo();
-//   console.log('Add book')
-// } 
-
-// const addComic = () => {
-//   const comic = new Comic('Batman', 'bob kane', 200, 5, '1939', 'Bob kane', 'DC Comics', 1);
-//   bookstore1.setComics = comic.getInfo();
-//   console.log('Add Comic')
-// }
-
-// const showProducts = () => {
-//   console.log('Books')
-//   console.log(bookstore1.getBooks);
-
-//   console.log('Comics')
-//   console.log(bookstore1.getComics);
-// }
-
-const sidebar = document.getElementById('sidebar');
-const menu = ['Show Books', 'Show Comics', 'Add Book', 'Add Comic'];
-
-const printBookstoreName = () => {
-  sidebar.insertAdjacentHTML(
-    'afterbegin',
-    `<h2 class="title">
-      ${bookstore1.getName}
-    </h2>`
-  )
-}
-
-const printMenu = () => {
-  sidebar.insertAdjacentHTML(
-    'beforeend',
-    `<ul class="list"></ul>`
-  )
-  
-  const list = document.querySelector('.list');
-  
-  for(let i = 0; i < menu.length; i++) {
-    list.insertAdjacentHTML(
-      'beforeend',
-      `<li>${menu[i]}</li>` 
+  const printBookstoreName = () => {
+    sidebar.insertAdjacentHTML(
+      'afterbegin',
+      `<h2 class="title">
+        <img src="./img/logo.svg" class="logo"/>
+        ${bookstore1.getName}
+      </h2>`
     )
   }
-}
+  
+  const createMenu = () => {
+    let menuHtml = '';
+    menu.forEach((elem, index) => {
+      menuHtml += `<li class="menu-item"> 
+        <a href="#" id="${index}">${elem}</a>
+      </li>`
+    });
+  
+    return menuHtml;
+  }
+  
+  const displayMenu = () => {
+    sidebar.insertAdjacentHTML(
+      'beforeend',
+      `<ul class="menu">${createMenu()}</ul>`
+    );
+  }
+  
+  const menuFunctionality = () => {
+    const items = document.querySelectorAll('.menu-item');
+    items.forEach((item) => {
+      item.addEventListener('click', (event) => {
+        event.preventDefault(); 
+  
+        const { id } = event.target
+        switch (id) {
+          case '0':
+            showBooks();
+            break;
+          case '1':
+            showComics();
+            break;
+          case '2':
+            addBooks();
+            break;
+          case '3':
+            addComics();
+            break;
+          default:
+            console.log('That option does not exist');
+            break;
+        }
+      });
+    });
+  }
+  
 
-printBookstoreName();
-printMenu();
+  const showBooks = () => {
+  
+    let books = bookstore1.getBooks;
+    let aux = '';
 
-// ** TAREA **
-// Crear una función que se encargué de crear el menu, iterando sobre la lista menu (línea 125)
-// Debe generarse un element html tipo ul, y dentro de ese ul un li para cada elemento lista; Puede ver el ejemplo de lo que debe crear en el html 
+    mainContent.innerHTML = "";
+
+    books.forEach((book) => {
+      aux +=
+      `<li>Title: ${book.title}</li>
+      <li>Author: ${book.author}</li>
+      <li>Price: ${book.price}</li>
+      <li>Year: ${book.year}</li>`
+    })
+  
+    mainContent.insertAdjacentHTML (
+      'beforeend',
+      `<ul>${aux}</ul>`
+    )
+  }
+  
+  const showComics = () => {
+  
+    let comics = bookstore1.getComics;
+    let aux = '';
+
+    mainContent.innerHTML = "";
+
+    comics.forEach((comic) => {
+      aux +=
+      `<li>Title: ${comic.title}</li>
+      <li>Author: ${comic.author}</li>
+      <li>Price: ${comic.price}</li>
+      <li>Illustrator: ${comic.illustrator}</li>
+      <li>Publisher: ${comic.publisher}</li>
+      <li>Volume: ${comic.volume}</li>
+      <li>Year: ${comic.year}</li>`
+    })
+  
+    mainContent.insertAdjacentHTML (
+      'beforeend',
+      `<ul>${aux}</ul>`
+    )
+  }
+  
+  const addBooks = () => {
+    const title = prompt('Title');
+    const author = prompt('Author');
+    const price = prompt('Price');
+    const stock = prompt('Stock');
+    const year = prompt('Year');
+
+    if(!title || !author || !price || !stock || !year) {
+      alert("Please do not enter empty values!");
+    }
+  
+    const book = new Book(title, author, price, stock, year);
+
+    let bookList = bookstore1.getBooks;
+    bookList.forEach((item) => {
+      if (title === item.title) {
+        alert("This book already exist");
+      }
+    });
+
+    bookstore1.setBooks = book.getInfo();
+  }
+  
+  const addComics = () => {
+    const title = prompt('Title');
+    const author = prompt('Author');
+    const price = prompt('Price');
+    const stock = prompt('Stock');
+    const year = prompt('Year');
+    const illustrator = prompt('Illustrator');
+    const publisher = prompt('Publisher');
+    const volume = prompt('Volume');
+
+    if(!title || !author || !price || !stock || !year || !illustrator || !publisher || !volume) {
+      alert("Please do not enter empty values!");
+    }
+
+    const comic = new Comic(title, author, price, stock, year, illustrator, publisher, volume);
+    
+    let comicList = bookstore1.getComics;
+    comicList.forEach((item) => {
+      if (title === item.title) {
+        alert("This comic already exist");
+      }
+    });
+
+    bookstore1.setComics = comic.getInfo();
+  }
+
+  initialize();
+})();
+
+//Hecho con ayuda de Heylin Loaiza.
